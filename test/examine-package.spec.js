cb97e8c231d597818b0b22bb00cbf1dd64792024
Fredrik Bostrom <--global>
2019-08-08 09:20:08 +0200

    Don't enfore the use of babel cli

--- test/examine-package.spec.js
+++ test/examine-package.spec.js
@@ -87,19 +87,6 @@ describe('examine-package.json', function() {
         expect(test.passed).to.be.false;
     });
 
-    it('should work for @babel/cli', function() {
-        const results = doctor({
-            packagePath: '/test/mocks/package_eslint_4_0_1.json',
-            silent: true
-        });
-        // console.log(results.tests);
-        const test = results.tests.find(item => {
-            console.log(item.desc);
-            return item.desc === 'should be using @babel/cli';
-        });
-        expect(test.passed).to.be.true;
-    });
-
     it('should not work for babel-cli', function() {
         const results = doctor({
             packagePath: '/test/mocks/package_eslint_4_0_0.json',
